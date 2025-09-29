d4ada701af02d65cefdb0d3aab4bffb9833b71c0
Dnlmzdjcb <rasdmywmm@gmail.com>
2025-09-29 12:38:53 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,118 +1,69 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const Flexbox = () => {
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.screen}>
+      <View style={styles.tpContainer}>
+        <View style={styles.blackBox} />
+        <View style={styles.yellowBox} />
+        <View style={styles.blackBox} />
+      </View>
+
+      <Image
+        source={require('./images/logo-with-motto-3.png')}
+        style={styles.img}
+      />
+
+      <View style={styles.btmContainer}>
+        <View style={styles.blackBox} />
+        <View style={styles.yellowBox} />
+        <View style={styles.blackBox} />
+      </View>
     </View>
   );
-}
+};
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
+export default Flexbox;
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  screen: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'space-between',
+    alignItems: 'center',
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  tpContainer: {
+    backgroundColor: 'red',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    padding: 20,
+    width: '100%',
+    columnGap: 10,
+    
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  btmContainer: {
+    backgroundColor: 'blue',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    padding: 20,
+    width: '100%',
   },
-  highlight: {
-    fontWeight: '700',
+  blackBox: {
+    backgroundColor: 'black',
+    height: 70,
+    width: 70,
+  },
+  yellowBox: {
+    backgroundColor: 'yellow',
+    height: 70,
+    width: 70,
+  },
+  img: {
+    height: 120,
+    width: 300,
+    resizeMode: 'contain',
+    marginVertical: 20,
   },
 });
-
-export default App;
